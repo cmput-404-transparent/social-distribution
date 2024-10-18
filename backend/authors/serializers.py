e2a5fcc45741905c4b8986b616f54683af784454
Logan Omelchuk <72324766+hamsandvich@users.noreply.github.com>
2024-10-18 03:05:30 -0600

    added AuthorSummarySerializer(

--- backend/authors/serializers.py
+++ backend/authors/serializers.py
@@ -27,3 +27,23 @@ class AuthorSerializer(serializers.Serializer):
         instance.username = validated_data.get('username', instance.username)
         instance.save()
         return instance
+    
+    
+class AuthorSummarySerializer(serializers.ModelSerializer):
+    type = serializers.CharField(default='author', read_only=True)
+    id = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
+    host = serializers.URLField()
+    displayName = serializers.CharField(source='display_name')
+    github = serializers.URLField(allow_blank=True, required=False)
+    profileImage = serializers.URLField(source='profile_image', allow_blank=True, required=False)
+
+    class Meta:
+        model = Author
+        fields = ['type', 'id', 'url', 'host', 'displayName', 'github', 'profileImage']
+
+    def get_id(self, obj):
+        return f"{obj.host}authors/{obj.id}/"
+
+    def get_url(self, obj):
+        return self.get_id(obj)
