c6b5f8727618137be2dfca003f3b2864c4c0f705
Tammy Young <tammy.q.young@gmail.com>
2024-10-19 18:31:36 -0600

    feat: fix migration

--- backend/posts/migrations/0002_post_github_activity_id.py
+++ backend/posts/migrations/0002_post_github_activity_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posts', '0003_post_shares_count'),
+        ('posts', '0001_initial'),
     ]
 
     operations = [
