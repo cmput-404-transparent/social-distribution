72b63deee2bae6857ae127a043b6636a185cba73
“Jassidak <jassidaksingh@gmail.com>
2024-10-30 16:01:52 -0600

    feat:  Updated AuthorAdmin to auto-fill host and page fields and hash passwords

--- backend/authors/admin.py
+++ backend/authors/admin.py
@@ -6,7 +6,18 @@ class AuthorAdmin(admin.ModelAdmin):
         # Hash the password if it has been set or changed
         if form.cleaned_data.get('password'):
             obj.set_password(form.cleaned_data['password'])
+        
+        # Save the object first to get the ID
         super().save_model(request, obj, form, change)
+
+        # Automatically set the host and page fields if they are not provided
+        if not obj.host:
+            obj.host = f"http://localhost:3000/api"
+        if not obj.page:
+            obj.page = f"{obj.host}/authors/{obj.id}"
+        
+        # Save again to update the host and page fields
+        obj.save()
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Follow)
