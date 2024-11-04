dccb241b78814568d79e68d43d8f558a6203592e
Tammy Young <tammy.q.young@gmail.com>
2024-11-04 12:12:40 -0700

    feat: set nav bar item as active by checking the path of the current page instead of using onclick

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -10,7 +10,6 @@ import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function NavBar() {
-  const [activeButton, setActiveButton] = useState(''); 
 
   const getAuthorId = () => {
     return localStorage.getItem('authorId');
@@ -20,7 +19,6 @@ export default function NavBar() {
   const location = useLocation();
 
   function toProfile() {
-    setActiveButton('profile'); 
     let link = "/authors/" + getAuthorId();
     navigate(link);
   }
@@ -40,36 +38,36 @@ export default function NavBar() {
           <p className="md:hidden">&lt;/&gt;</p>
         </div>
 
-        <NavLink to="/stream" className="align-middle grid grid-flow-col auto-cols-max" onClick={() => setActiveButton('stream')}>
-          <span className={`flex items-center justify-left w-30 p-4 rounded-full transition duration-300 ${activeButton === 'stream' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
+        <NavLink to="/stream" className="align-middle grid grid-flow-col auto-cols-max">
+          <span className={`flex items-center justify-left w-30 p-4 rounded-full transition duration-300 ${location.pathname === '/stream' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
             <HomeIcon />
             <p className="hidden md:inline pl-3">Stream</p>
           </span>
         </NavLink>
 
-        <NavLink to="/search" className="align-middle grid grid-flow-col auto-cols-max" onClick={() => setActiveButton('search')}>
-          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${activeButton === 'search' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
+        <NavLink to="/search" className="align-middle grid grid-flow-col auto-cols-max">
+          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${location.pathname === '/search' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
             <SearchIcon />
             <p className="hidden md:inline pl-3">Search</p>
           </span>
         </NavLink>
 
-        <NavLink to="/make-post" className="align-middle grid grid-flow-col auto-cols-max" onClick={() => setActiveButton('post')}>
-          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${activeButton === 'post' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
+        <NavLink to="/make-post" className="align-middle grid grid-flow-col auto-cols-max">
+          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${location.pathname === '/make-post' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
             <ControlPointIcon />
             <p className="hidden md:inline pl-3">Post</p>
           </span>
         </NavLink>
 
-        <NavLink to="/notifications" className="align-middle grid grid-flow-col auto-cols-max" onClick={() => setActiveButton('notifications')}>
-          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${activeButton === 'notifications' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
+        <NavLink to="/notifications" className="align-middle grid grid-flow-col auto-cols-max">
+          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${location.pathname === '/notifications' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
             <CircleNotificationsIcon />
             <p className="hidden md:inline pl-3">Notifications</p>
           </span>
         </NavLink>
 
         <div className="align-middle grid grid-flow-col auto-cols-max cursor-pointer" onClick={toProfile}>
-          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${activeButton === 'profile' ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
+          <span className={`relative flex items-center justify-center p-4 rounded-full transition duration-300 ${location.pathname === '/authors/' + getAuthorId() ? 'bg-orange-500/20' : 'hover:bg-orange-500/20'}`}>
             <AccountCircleIcon />
             <p className="hidden md:inline pl-3">Profile</p>
           </span>
