9adcc7be01d0e8aad781e7dca589dff12c8132ab
Tammy Young <tammy.q.young@gmail.com>
2024-11-12 13:55:56 -0700

    feat: move fqid endpoints to the bottom of post urls

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
+    
+    # Post GitHub activity for a specific author
+    path('<int:author_id>/github/', views.post_github_activity, name="post_github"),
+    
+    # Get an image post by its fqid
+    path("<str:fqid>/image/", views.get_image_post_by_fqid, name="get_image_post_by_fqid"),
+
+    # BY FQID ENDPOINTS --------------------------------------------------------------------
 
     # Get image from a post by post fqid
     path("<path:post_fqid>/image/", views.get_post_image, name="get_post_image"),
@@ -13,10 +21,4 @@ urlpatterns = [
 
     # Get a specific post by its fqid
     path("<path:post_fqid>/", views.get_post, name="get_post"),
-    
-    # Post GitHub activity for a specific author
-    path('<int:author_id>/github/', views.post_github_activity, name="post_github"),
-    
-    # Get an image post by its fqid
-    path("<str:fqid>/image/", views.get_image_post_by_fqid, name="get_image_post_by_fqid")
 ]
